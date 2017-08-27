87288a702f05ff58fdbda9ac8e4ae534abb816b4
Uchiko <memememomo@gmail.com>
2017-08-27 11:08:28 +0900

    本数を計算するメソッドを実装

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
+  BONUS_AMOUNT=3
+
   def self.total_count_for(amount)
-    0
+    amount + calculate_bonus(amount)
+  end
+
+  def self.calculate_bonus(amount)
+    bonus = amount / BONUS_AMOUNT
+    rest = amount % BONUS_AMOUNT
+    if bonus == 0
+      return 0
+    else
+      bonus + calculate_bonus(bonus+rest)
+    end
   end
 end
